95f8b3e70c8e2201150b6c9b7f658c8bde8a80d3
damiancclarke <damian.c.clarke@gmail.com>
2016-01-18 23:02:25 -0300

    Adding function for formatting resutls

--- Do/worldTabs.py
+++ Do/worldTabs.py
@@ -21,12 +21,30 @@ OUT  = "/home/damiancclarke/investigacion/Activa/Twins/paper/twinsHealth/tex/"
 #==============================================================================
 foot = "$^{*}$p$<$0.1; $^{**}$p$<$0.05; $^{***}$p$<$0.01"
 
+def formatLine(line):
+    beta = float(line.split(";")[2])
+    se   = float(line.split(";")[3])
+    lCI  = str(float(line.split(";")[5]))
+    uCI  = str(float(line.split(";")[4]))
+
+    t    = abs(beta/se)
+    if t>2.576:
+        beta = str(beta)+'$^{***}$'
+    elif t>1.96:
+        beta = str(beta)+'$^{**}$'
+    elif t>1.645:
+        beta = str(beta)+'$^{*}$'
+    else:
+        beta=str(beta)
+    
+    lineOUT = beta + '&[' + lCI + ',' + uCI + ']'
+    return lineOUT
 
 #==============================================================================
 #== (2a) Write Conditional Unstandardised 
 #==============================================================================
 DHSi = open(RIN + 'worldEstimatesDHS.csv').readlines()
-#USAi = open(RIN + ).readlines()
+USAi = open(RIN +    'worldEstimates.csv').readlines()
 #SWEi = open(RIN + ).readlines()
 #CHIi = open(RIN + ).readlines()
 #UKSi = open(RIN + ).readlines()
@@ -35,7 +53,7 @@ tabl = open(OUT + 'twinEffectsCond.tex', 'w')
 
 tabl.write('\\begin{spacing}{1}\n\n \\begin{table}[htpb!]\n'
            '\\begin{center}\n\\caption{Twin Effects}\n'
-           '\\begin{tabular}{lcclcc}\n \\toprule'
+           '\\begin{tabular}{llcllc}\n \\toprule'
            '\\multicolumn{3}{c}{Health Behaviours} &'
            '\\multicolumn{3}{c}{Health Conditions} \\\\ \n'
            '\\cmidrule(r){1-3} \\cmidrule(r){4-6} \n'
@@ -44,27 +62,61 @@ tabl.write('\\begin{spacing}{1}\n\n \\begin{table}[htpb!]\n'
            '\\rowcolor{LightCyan} \\multicolumn{6}{c}'
            '{\\textbf{Panel A: United States Birth Certificates}} \\\\ \n')
 
+for i,line in enumerate(USAi):
+    if i>0 and i<13:
+        line = formatLine(line)
+        if i==1:
+            n1 = 'Mother\'s Height&'   + line + '\\\\'
+        elif i==4:
+            n2 = 'Smoked Trimester 1&' + line + '&'
+        elif i==5:
+            n3 = 'Smoked Trimester 2&' + line + '&'
+        elif i==6:
+            n4 = 'Smoked Trimester 3&' + line + '&'
+        elif i==7:
+            n5 = 'Diabetes (pre)&'     + line + '\\\\'
+        elif i==8:
+            n6 = 'Gestation Diabetes&' + line + '\\\\'
+        elif i==9:
+            n7 = 'Eclampsia&'          + line + '\\\\'
+        elif i==10:
+            n8 = 'Hypertension (pre)&' + line + '\\\\'
+        elif i==11:
+            n9 = 'Gestation Hyperten&' + line + '\\\\'
+tabl.write(n2+n1+'\n' + n3+n5+'\n' + n4+n8+'\n' + '&&&'+n6+'\n' 
+           + '&&&'+n7+'\n' + '&&&'+n9+'\n')
+
+
 tabl.write('\\rowcolor{LightCyan} \\multicolumn{6}{c}'
            '{\\textbf{Panel B: Pooled Demographic and Health Surveys}} \\\\ \n')
 for i,line in enumerate(DHSi):
-    if i==1: 
-        CI = '[' + str(line.split(';')[5]) + str(line.split(';')[4]) + ']'
-        n1 = 'Mother\'s Height&'+str(line.split(';')[2]) + '&' + CI + '&'
-    elif i==2: 
-        CI = '[' + str(line.split(';')[5]) + str(line.split(';')[4]) + ']'
-        n2 = 'Mother\'s BMI&'+str(line.split(';')[2]) + '&' + CI + '&'
-    elif i==4: 
-        CI = '[' + str(line.split(';')[5]) + str(line.split(';')[4]) + ']'
-        n3 = 'Doctor Availability&'+str(line.split(';')[2]) + '&' + CI + '\\\\'
-    elif i==5: 
-        CI = '[' + str(line.split(';')[5]) + str(line.split(';')[4]) + ']'
-        n4 = 'Nurse Availability&'+str(line.split(';')[2]) + '&' + CI + '\\\\'
-    elif i==6: 
-        CI = '[' + str(line.split(';')[5]) + str(line.split(';')[4]) + ']'
-        n5 = 'No Prenatal Care&'+str(line.split(';')[2]) + '&' + CI + '\\\\'
+    if i>0 and i<7:
+        line = formatLine(line)
 
-    print line
+        if i==1: 
+            n1 = 'Mother\'s Height&'   + line + '&'
+        elif i==2: 
+            n2 = 'Mother\'s BMI&'      + line + '&'
+        elif i==4: 
+            n3 = 'Doctor Availability&'+ line + '\\\\'
+        elif i==5: 
+            n4 = 'Nurse Availability&' + line + '\\\\'
+        elif i==6: 
+            n5 = 'No Prenatal Care&'   + line + '\\\\'
+
 tabl.write(n1+n3 + '\n' + n2+n4+ '\n' + '&&&'+n5+'\n')
+
+tabl.write('\\rowcolor{LightCyan} \\multicolumn{6}{c}'
+           '{\\textbf{Panel C: Swedish Medical Birth Registry}} \\\\ \n')
+
+tabl.write('\\rowcolor{LightCyan} \\multicolumn{6}{c}'
+           '{\\textbf{Panel D: Chilean Survey of Early Infancy}} \\\\ \n')
+
+tabl.write('\\rowcolor{LightCyan} \\multicolumn{6}{c}'
+           '{\\textbf{Panel E: Somerset Birth Survey (United Kingdom)}} \\\\ \n')
+
+
+
 
 
 tabl.write('\\bottomrule \n \\end{tabular} \n \\end{center} \\end{table} \n'
